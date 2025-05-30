451b8694e3fccc6c3398eef2640231f45d1c7873
Raja Mannan Khan <159701740+RajaHadi@users.noreply.github.com>
2025-05-30 10:19:09 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from agents import Agent, Runner, AsyncOpenAI, OpenAIChatCompletionsModel, RunConfig
+from openai_agents import Agent, Runner, AsyncOpenAI, OpenAIChatCompletionsModel, RunConfig
 from dotenv import load_dotenv
 import streamlit as st
 import asyncio
